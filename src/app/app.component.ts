9fca5fe21131a0b0b8f38d1ee7617718b2d57801
pranav <pranavverma550@gmail.com>
2019-04-27 19:34:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulartemplatedrivenform';
+  
+  Register(regForm:any){
+    var firstname=regForm.controls.firstname.value;
+    var lastname=regForm.controls.lastname.value;
+    var email=regForm.controls.email.value;
+  console.log(regForm)
+  }
 }
